7d1cb1c3ca50d11ab67f81c92cceb7f24c662793
Aashutosh17 <78782279+Aashutosh17@users.noreply.github.com>
2023-01-15 19:31:03 +0545

    api in dart

---
+++ lib/model/batch.dart
@@ -0,0 +1,30 @@
+import 'package:batch_student_objbox_api/model/student.dart';
+import 'package:json_annotation/json_annotation.dart';
+import 'package:objectbox/objectbox.dart';
+
+//flutter pub run build_runner build  --delete-conflicting-outputs
+part 'batch.g.dart';
+
+@JsonSerializable()
+@Entity()
+class Batch {
+  @Id(assignable: true)
+  int id;
+
+  @Unique()
+  @JsonKey(name: '_id')
+  String? batchId;
+  String? batchName;
+
+  factory Batch.fromJson(Map<String, dynamic> json) {
+    return Batch(json['_id'], json['batchName']);
+  }
+
+  Map<String, dynamic> toJson() => {'_id': batchId, 'batchName': batchName};
+
+  @Backlink()
+  final student = ToMany<Student>();
+
+  //Constructor
+  Batch(this.batchId, this.batchName, {this.id = 0});
+}
